6ebe49a0b5bfef3d8f5609a183fb537260585ae2
Neba Asombang <Neba.Asombang@ajbell.co.uk>
2021-08-11 08:39:16 +0100

    chore: Initial Commit

---
+++ Source/CodeKata.Kata13.Console/Services/ILineCountingService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeKata.Kata13.Console.Services
+{
+    public interface ILineCountingService
+    {
+    }
+}
---
+++ Tests/CodeKata.Kata13.Console.Tests/LineCounterServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CodeKata.Kata13.Console.Tests
+{
+    public class LineCounterServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
